f01272f3cea916a04b3e49da54546de95f755774
stoney <sarkisyanmihail11@gmail.com>
2025-02-07 03:00:20 +0400

    Working category field + submition changes

--- src/addPage.py
+++ src/addPage.py
@@ -71,7 +71,6 @@ class AddWindow(QWidget):
         self.setContextMenuPolicy(Qt.ContextMenuPolicy.NoContextMenu)
 
         definitionLabel = QLabel("Definition")
-        # create a CustomTextEdit and pass gatherInfo as the callback for enter.
         self.definitionInput = CustomTextEdit(nextWidget = self.categoryInput, parent = self)
         self.definitionInput.setPlaceholderText("Type something...")
         self.definitionInput.setContextMenuPolicy(Qt.ContextMenuPolicy.NoContextMenu)
@@ -84,22 +83,22 @@ class AddWindow(QWidget):
 
         enterButton = QPushButton("Submit")
         enterButton.clicked.connect(self.gatherInfo)
-        enterButton.setStyleSheet("font-size: 15px;")
+        enterButton.setStyleSheet("font-size: 14px;")
 
         # Just for grid expansion.
         expander = QWidget()
         expander.setStyleSheet("background: transparent;")
         expander.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
 
-        grid.addWidget(returnButton, 0, 0, 1, 1)
+        grid.addWidget(returnButton, 0, 0)
         grid.addWidget(info, 1, 0, 1, 2)
         grid.addWidget(wordLabel, 2, 0)
         grid.addWidget(self.wordInput, 3, 0, 1, 2)
         grid.addWidget(definitionLabel, 4, 0, 1, 0)
         grid.addWidget(self.definitionInput, 5, 0, 2, 4)
-        grid.addWidget(self.categoryInput, 7, 0, 1, 1)
+        grid.addWidget(self.categoryInput, 7, 0, 1, 2)
         grid.addWidget(enterButton, 7, 2)
-        grid.addWidget(expander, 0, 3, 5, 2)
+        grid.addWidget(expander, 0, 2, 5, 2)
 
         self.setLayout(grid)
 
@@ -118,6 +117,7 @@ class AddWindow(QWidget):
 
         self.wordInput.clear()
         self.definitionInput.clear()
+        self.wordInput.setFocus()
 
 
     def paintEvent(self, event):
---
+++ src/modePage.py
@@ -0,0 +1,12 @@
+"""
+TODO: 
+    this page is to choose game mode.
+    main idea:
+        choose how many words will be in quiz
+        choose category from which words will be gathered
+
+        preferably: hard mode (you're limited in time) and background changes color to red
+        time can be chosen manually, category will be ALL by default
+
+        hmm other ideas later
+"""
