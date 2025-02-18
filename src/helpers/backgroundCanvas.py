d9a698153b01f184dd78dffb17a24adda43c1b4c
stoney <sarkisyanmihail11@gmail.com>
2025-02-19 00:51:12 +0400

    separated colors to json file

--- src/helpers/backgroundCanvas.py
+++ src/helpers/backgroundCanvas.py
@@ -2,12 +2,36 @@ from PyQt6.QtWidgets import QApplication, QWidget, QGraphicsBlurEffect
 from PyQt6.QtCore import Qt, QPointF
 from PyQt6.QtGui import QPainter, QBrush, QColor, QLinearGradient, QRadialGradient, QPixmap
 import sys
+import json
 
+# TODO : maybe I'll have to add one more function that will open the window again after pressing one button on the main page
 
 class BackgroundCanvas(QWidget):
     def __init__(self):
         super().__init__()
+        self.colorsDict = {}
+        self.loadColors()
         self.UI()
+
+
+    def loadColors(self):
+        self.colorsDict = self.parseDefaultColors()
+        self.colorsDict.update(self.parseUserColors())
+
+
+    def parseDefaultColors(self):
+        with open("helpers/defaultColors.json", "r") as defaultData:
+            defaultColors = json.load(defaultData)
+
+        return defaultColors
+
+
+    def parseUserColors(self):
+        with open("helpers/userColors.json", "r") as userData:
+            userColors = json.load(userData)
+
+        return userColors
+
 
     def UI(self):
         self.setFixedSize(400, 450)
@@ -20,29 +44,35 @@ class BackgroundCanvas(QWidget):
         painter.setPen(Qt.GlobalColor.transparent)
 
         # background
-        gradient = QLinearGradient(QPointF(self.width(), 0), QPointF(0, self.height()))
-        gradient.setColorAt(1, QColor("#388087"))
-        gradient.setColorAt(0, QColor("#6FB3B8"))
+        gradient = QLinearGradient(QPointF(self.width(), 0), QPointF(0, self.height())) 
+        gradient.setColorAt(1, QColor(self.colorsDict["background1"][0], self.colorsDict["background1"][1],
+                                      self.colorsDict["background1"][2], self.colorsDict["background1"][3]))
+        gradient.setColorAt(0, QColor(self.colorsDict["background0"][0], self.colorsDict["background0"][1],
+                                      self.colorsDict["background0"][2], self.colorsDict["background0"][3]))
 
         brush = QBrush(gradient)
         painter.setBrush(brush)
         painter.drawRect(0, 0, self.width(), self.height())
 
         # big circle
-        circle = QRadialGradient(QPointF(0, 0), 500)
-        circle.setColorAt(1, QColor(185, 248, 255, 120))
-        circle.setColorAt(0, QColor("#C2EDCE"))
+        bigCircle = QRadialGradient(QPointF(0, 0), 500)
+        bigCircle.setColorAt(1, QColor(self.colorsDict["bigCircle1"][0], self.colorsDict["bigCircle1"][1],
+                                       self.colorsDict["bigCircle1"][2], self.colorsDict["bigCircle1"][3]))
+        bigCircle.setColorAt(0, QColor(self.colorsDict["bigCircle0"][0], self.colorsDict["bigCircle0"][1],
+                                       self.colorsDict["bigCircle0"][2], self.colorsDict["bigCircle0"][3]))
 
-        brush = QBrush(circle)
+        brush = QBrush(bigCircle)
         painter.setBrush(brush)
         painter.drawEllipse(self.width() - 700, self.height() - 350, 500, 500)
 
         # small circle
-        radialGradient = QRadialGradient(QPointF(self.width() - 30, 30), 100)
-        radialGradient.setColorAt(0, QColor("#EBF7FA")) 
-        radialGradient.setColorAt(1, QColor("#B9F8FF")) 
+        smallCircle = QRadialGradient(QPointF(self.width() - 30, 30), 100)
+        smallCircle.setColorAt(0, QColor(self.colorsDict["smallCircle0"][0], self.colorsDict["smallCircle0"][1],
+                                         self.colorsDict["smallCircle0"][2], self.colorsDict["smallCircle0"][3]))  
+        smallCircle.setColorAt(1, QColor(self.colorsDict["smallCircle1"][0], self.colorsDict["smallCircle1"][1],
+                                         self.colorsDict["smallCircle1"][2], self.colorsDict["smallCircle1"][3]))
 
-        brush = QBrush(radialGradient)
+        brush = QBrush(smallCircle)
         painter.setBrush(brush)
         painter.drawEllipse(self.width() - 100, 30, 80, 80)
 
@@ -54,4 +84,3 @@ if __name__ == "__main__":
     canvas = BackgroundCanvas()
     canvas.show()
     sys.exit(app.exec())
-
--- src/mainPage.py
+++ src/mainPage.py
@@ -87,3 +87,4 @@ if __name__ == "__main__":
     window = Window()
     window.show()
     sys.exit(app.exec())
+
